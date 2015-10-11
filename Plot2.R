b2f171f6f855276d6aed5e74651bb976679c9107
Ganesh Rajagopal <gans.rajagopal@gmail.com>
2015-10-11 18:27:10 -0400

    Updated wth code 32 and PNG Files

---
+++ Plot2.R
@@ -0,0 +1,23 @@
+# Read the Data from the file 
+power_cdata <- read.table("C:\\R\\CourseraR\\Exploratory_data_analysis\\household_power_consumption.txt", header=T, sep=';')
+
+# Format the input date to a DAte format 
+power_cdata$Date <- as.Date(power_cdata$Date, "%d/%m/%Y")
+
+# Set the Filter Date to a Date Format 
+filter_date <- as.Date(c("2007-02-01","2007-02-02"), "%Y-%m-%d")
+
+# Filter out the data to be plotted.  
+
+final_data <- subset(power_cdata, Date %in% filter_date)
+
+# Filter out the data to be plotted.  
+## Test out to see the graph comes fine: 
+## hist(as.numeric(final_data$Global_active_power), col="red")
+
+## Write the output to a Plot1.png file
+
+dt= strptime(paste(final_data$Date, final_data$Time), "%d/%m/%Y %H:%M:%S")
+png("C:\\R\\CourseraR\\Exploratory_data_analysis\\Plot2.png", width=480, height=480)
+plot(dt, as.numeric(final_data$Global_active_power), type="1", xlab="", ylab="Global Active Power(KW)")
+dev.off()
